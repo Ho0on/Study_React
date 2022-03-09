58e6fd79b82ed76110213cb432c122ce3dc6b4a3
SangHoon <joa3668@gmail.com>
2022-03-09 20:52:35 +0900

    ✨ [Feat] redux-actions 적용

--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,31 +1,52 @@
-// 액션 타입 정의
-const SHOW_ALL = 'study_react/filter/SHOW_ALL';
-const SHOW_COMPLETE = 'study_react/filter/SHOW_COMPLETE';
+import { handleActions } from 'redux-actions';
+import { createActions } from 'redux-actions';
 
-// 액션 생성 함수
-export const showAll = () => {
-  return { type: SHOW_ALL };
-};
+// // 액션 타입 정의
+// const SHOW_ALL = 'study_react/filter/SHOW_ALL';
+// const SHOW_COMPLETE = 'study_react/filter/SHOW_COMPLETE';
 
-export const showComplete = () => {
-  return { type: SHOW_COMPLETE };
-};
+// // 액션 생성 함수
+// export const showAll = () => {
+//   return { type: SHOW_ALL };
+// };
+
+// export const showComplete = () => {
+//   return { type: SHOW_COMPLETE };
+// };
 
 // 초기값
 const initialState = 'ALL';
 
-// 리듀서
-const reducer = (previousState = initialState, action) => {
-  switch (action.type) {
-    case SHOW_COMPLETE:
-      return 'COMPLETE';
+// // 리듀서
+// const reducer = (previousState = initialState, action) => {
+//   switch (action.type) {
+//     case SHOW_COMPLETE:
+//       return 'COMPLETE';
 
-    case SHOW_ALL:
-      return 'ALL';
+//     case SHOW_ALL:
+//       return 'ALL';
 
-    default:
-      return previousState;
+//     default:
+//       return previousState;
+//   }
+// };
+
+// redux-actions : ducks pattern을 사용할떄 편하게 적용가능
+export const { showAll, showComplete } = createActions(
+  'SHOW_ALL',
+  'SHOW_COMPLETE',
+  {
+    prefix: 'study_react/filter',
   }
-};
+);
+
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => 'ALL',
+    SHOW_COMPLETE: () => 'COMPLETE',
+  },
+  initialState,
+  { prefix: 'study_react/filter' }
+);
 
 export default reducer;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,36 +1,69 @@
-// 액션 타입 정의
-const ADD_TODO = 'study_react/todos/ADD_TODO';
-const COMPLETE_TODO = 'study_react/todos/COMPLETE_TODO';
+import { handleActions } from 'redux-actions';
+import { createActions } from 'redux-actions';
 
-// 액션 생성 함수
-export const addTodo = text => {
-  return { type: ADD_TODO, text };
-};
+// // 액션 타입 정의
+// const ADD_TODO = 'study_react/todos/ADD_TODO';
+// const COMPLETE_TODO = 'study_react/todos/COMPLETE_TODO';
 
-export const completeTodo = index => {
-  return { type: COMPLETE_TODO, index };
-};
+// // 액션 생성 함수
+// export const addTodo = text => {
+//   return { type: ADD_TODO, text };
+// };
+
+// export const completeTodo = index => {
+//   return { type: COMPLETE_TODO, index };
+// };
 
 // 초기값
 const initialState = [];
 
-//리듀서
-const reducer = (previousState = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...previousState, { text: action.text, done: false }];
+// //리듀서
+// const reducer = (previousState = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return [...previousState, { text: action.text, done: false }];
 
-    case COMPLETE_TODO:
-      return previousState.map((todo, index) => {
-        if (index === action.index) {
+//     case COMPLETE_TODO:
+//       return previousState.map((todo, index) => {
+//         if (index === action.index) {
+//           return { ...todo, done: true };
+//         }
+//         return todo;
+//       });
+
+//     default:
+//       return previousState;
+//   }
+// };
+
+// redux-actions
+export const { addTodo, completeTodo } = createActions(
+  {
+    ADD_TODO: text => ({ text }),
+    COMPLETE_TODO: index => ({ index }),
+  },
+  {
+    prefix: 'study_react/todos',
+  }
+);
+
+const reducer = handleActions(
+  {
+    ADD_TODO: (state, action) => {
+      return [...state, { text: action.payload.text, done: false }];
+    },
+
+    COMPLETE_TODO: (state, action) => {
+      return state.map((todo, index) => {
+        if (index === action.payload.index) {
           return { ...todo, done: true };
         }
         return todo;
       });
-
-    default:
-      return previousState;
-  }
-};
+    },
+  },
+  initialState,
+  { prefix: 'study_react/todos' }
+);
 
 export default reducer;
